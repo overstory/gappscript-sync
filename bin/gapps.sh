20e46b7ab12f22678a4299e131192d456be5ec27
Arunas Vaitkus <arunas21@gmail.com>
2017-03-15 15:59:51 +0100

    remove webapp and create a new custom gradle task

--- bin/gapps.sh
+++ bin/gapps.sh
@@ -5,4 +5,4 @@ GAPPS_DIR="node_modules/node-google-apps-script/bin/gapps"
 COMMAND1=$1
 COMMAND2=$2
 
-${PWD}/webapp/${GAPPS_DIR} ${COMMAND1} ${COMMAND2}
+${PWD}/${GAPPS_DIR} ${COMMAND1} ${COMMAND2}
